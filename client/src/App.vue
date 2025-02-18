e0ad025a2d3c98a1e0f23b06e9a0d5815b031f0a
PrinceKex <ehigiator.ukeke@gmail.com>
2025-02-18 19:01:53 +0100

    front end code worked on

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,69 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+ <section className="w-auto flex flex-row h-screen">
+  <div className="flex flex-col w-1/2 bg-slate-100 h-auto m-2">
+   <div className="flex flex-col items-start justify-center py-4">
+    <img src="./assets/image 30.png" />
+   </div>
+   <div className="items-center justify-center">
+    <img src="./assets/image 31.png" />
+   </div>
+   <h1 className="text-4xl text-center font-extrabold pt-3">Team Achieve</h1>
+   <h2 className="text-3xl text-center font-bold pt-3">
+    Your Perfect Solution for funding your desires
+   </h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div className="flex flex-col w-1/2 bg-slate-200 h-auto">
+   <div className="flex flex-col items-center justify-normal pt-20">
+    <h1 className="text-4xl text-center font-extrabold pt-3">Welcome Back</h1>
+    <h2 className="text-2xl text-center font-semibold pt-3 px-auto">
+     Enter your email and password to access your account
+    </h2>
+   </div>
+   <div className="flex flex-col items-start justify-center w-full  pt-10">
+    <form action="#" className="w-full justify-evenly h-auto">
+     <div className="flex flex-col items-start justify-center w-full mt-5">
+      <label for="email" className="text-2xl">Email Address *</label>
+      <input
+       type="text"
+       placeholder="Enter your email"
+       className="w-full h-8"
+      />
+     </div>
+     <div className="flex flex-col items-start justify-center w-full mt-5">
+      <label for="password" className="text-2xl">Password *</label>
+      <input
+       type="text"
+       placeholder="Enter your password"
+       className="w-full h-8"
+      />
+     </div>
+    </form>
+   </div>
+   <div className="w-full flex flex-col items-center justify-end pt-36">
+    <button
+     className="bg-fuchsia-900 text-white w-full text-2xl t font-bold rounded-md"
+    >
+     Submit
+    </button>
+    <h1 className="text-xl font-semibold text-center">
+     Don't have an account? <span>Sign Up </span>
+    </h1>
+   </div>
+  </div>
+ </section>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+ height: 6em;
+ padding: 1.5em;
+ will-change: filter;
+ transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+ filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+ filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+</style> -->
